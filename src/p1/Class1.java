89052a41f01b401f959eec553690a69509e3124a
Umakant Sahani <24uma10@gmail.com>
2025-07-20 11:53:56 +0530

    Braches Changes

--- src/p1/Class1.java
+++ src/p1/Class1.java
@@ -1,9 +1,10 @@
 package p1;
 
 public class Class1 {
-	
+
 	public static void main(String[] args) {
-		System.out.println("P1 c1 world");
+		System.out.println("P1 c1 world TechnoCredits");
+		System.out.println("New Line");
 	}
 
 }
